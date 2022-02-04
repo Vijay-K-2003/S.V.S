0a5e8de4e903bb491c46696d393478e6b4d8336a
kewal-wq <kewal.delhiwala5@gmail.com>
2022-02-04 18:36:33 +0530

    Test map

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+mongoose.connect('mongodb://localhost:27017/svs')
+.then((res) => {
+    console.log("Database Connected");
+})
+.catch((err) => {
+    console.log("Database connected error", err);
+});
 
 const app = express();
 
-app.get('/', (rea, res) =>{
+app.get('/', (req, res) =>{
     res.send("Hey we made it!");
 })
 
-app.listen(4000, (req, res) =>{
+app.listen(4000, () =>{
     console.log("Listning to port 4000");
 })
 
---
+++ server/models/map.js
@@ -0,0 +1,12 @@
+
+// import mapboxgl from "../mapbox-gl/dist/mapbox-gl.js";
+
+
+ 
+//      mapboxgl.accessToken = 'pk.eyJ1Ijoia2V3YWxkZWxoaXdhbGEiLCJhIjoiY2t1dGo4dXkyMWh1YjJxcDVmdXI4OWFlYyJ9.ht2jl-uDv_SBHXE7DBaPvQ';
+//     const map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11',
+//   center: [-74.5, 40],
+//   zoom: 9
+// });
