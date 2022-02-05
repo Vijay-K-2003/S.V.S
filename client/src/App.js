81db98d3466cc3b12d88332781f50594329ff2cc
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-02-05 22:56:39 +0530

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import env from 'react-dotenv';
 
+// mapboxgl.accessToken = env.REACT_APP_API_TOKEN;
+// add access token
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  map.addControl(
+    new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true,
+      },
+      trackUserLocation: true,
+      showUserHeading: true,
+    })
+  );
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos.coords.latitude + "," + pos.coords.longitude);
+    });
+  } else {
+    console.log("You are not allowed to access location");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
---
+++ client/src/components/Map.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Map = () => {
+  return <div Id="Map">
+      
+  </div>;
+};
