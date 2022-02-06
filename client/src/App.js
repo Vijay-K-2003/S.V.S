70acb1d1c0b8c18d7fdc6d2468f28463f1a441b8
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-02-06 11:04:33 +0530

    add mapbox to client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,53 @@
 import React, { useRef, useEffect, useState } from "react";
 // import Map from "./components/Map.js";
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { NavigationControl } from "mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2V3YWxkZWxoaXdhbGEiLCJhIjoiY2t6OGgxcXVqMHZmdjJ2czh1OWljYjNscCJ9.Xssyc6T_KwRT3Gjbb_X9Qw';
-
-
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoia2V3YWxkZWxoaXdhbGEiLCJhIjoiY2t6OGgxcXVqMHZmdjJ2czh1OWljYjNscCJ9.Xssyc6T_KwRT3Gjbb_X9Qw";
 
 function App() {
   const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
 
-useEffect(() => {
-  if (map.current) return; // initialize map only once
-  map.current = new mapboxgl.Map({
-  container: mapContainer.current,
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [lng, lat],
-  zoom: zoom
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        setViewport({
+          ...viewport,
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        });
+      });
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
   });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
   });
-  
+
+
   return (
     <div>
-    <div ref={mapContainer} className="map-container" />
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
     </div>
-    );
-
-  }
+  );
+}
 export default App;
-  
