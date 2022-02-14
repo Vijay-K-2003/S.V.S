d3f1680683245f5cab209f8051daa836d13a7da5
Vijay Karanjkar <202051200@iiitvadodara.ac.in>
2022-02-14 14:43:13 +0530

    protect vendor route

--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { getVendor, createVendor } from "../controllers/vendors.js";
+import isLoggedIn from "../middleware.js";
 
 router.get("/", getVendor);
 
-router.post("/new", createVendor);
+router.post("/new", isLoggedIn, createVendor);
 
 export default router;
