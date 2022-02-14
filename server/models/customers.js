6c6f6803eddb9c0bf2d32f8af34138e80424fde7
Vijay Karanjkar <202051200@iiitvadodara.ac.in>
2022-02-14 23:41:03 +0530

    add vendor to customer schema

--- server/models/customers.js
+++ server/models/customers.js
@@ -2,27 +2,29 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const customerSchema = new Schema({
-    
-        name: {
-            type: String,
-            required: [true, "Name cannot be empty!"]
-        },
-        email: {
-            type: String,
-            required: [true, "Email cannot be empty!"]
-        },
-        mobileNumber: {
-            type: Number,
-            required: [true, 'Mobile Number cannot be empty!']
-        },
-       latitude: {
-           type: Number,
-            required: [true, 'latitude is required']
-       },
-       longitude: {
-        type: Number,
-        required: [true, 'longitude is required']
-    }
+  name: {
+    type: String,
+    required: [true, "Name cannot be empty!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email cannot be empty!"],
+  },
+  mobileNumber: {
+    type: Number,
+    required: [true, "Mobile Number cannot be empty!"],
+  },
+  latitude: {
+    type: Number,
+    required: [true, "latitude is required"],
+  },
+  longitude: {
+    type: Number,
+    required: [true, "longitude is required"],
+  },
+  myVendors: [{
+    type: Array,
+  }],
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getVendor, createVendor } from "../controllers/vendors.js";
+import { getVendor, createVendor, addVendor } from "../controllers/vendors.js";
 import isLoggedIn from "../middleware.js";
 
 router.get("/", getVendor);
+router.get("/customers/:id/allVendor/:venid", addVendor);
 
-router.post("/new", isLoggedIn, createVendor);
+router.post("/new", createVendor);
 
 export default router;
