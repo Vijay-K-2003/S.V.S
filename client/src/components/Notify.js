15a722a7b8fc80ea08b8f5ac39dea8925fb0dc32
kewal-wq <kewal.delhiwala5@gmail.com>
2022-04-03 11:24:27 +0530

    Fix https Error

--- client/src/components/Notify.js
+++ client/src/components/Notify.js
@@ -1,74 +1,53 @@
-import React, {useState, useEffect} from 'react';
-import {useParams} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
-import "../css/Notify.css"
-import notifylogo from "./assets/notifylogo.png"
+import "../css/Notify.css";
+import notifylogo from "./assets/notifylogo.png";
 
 const Notify = () => {
+  const [customer, setCustomer] = useState("");
+  const [vendor, setVendor] = useState("");
 
-    const [customer, setCustomer] = useState("");
-    const [vendor, setVendor] = useState("");
-    
+  const { id, venid } = useParams();
 
-    const {id, venid} = useParams();
-
-    useEffect(() => {
-      
+  useEffect(() => {
     const cust = async () => {
-        const res = await axios.get(`http://localhost:4000/customers/${id}`)
-        setCustomer(res.data);
-    }
+      const res = await axios.get(`http://localhost:4000/customers/${id}`);
+      setCustomer(res.data);
+    };
     cust();
-     
-    }, [id])
+  }, [id]);
 
-    useEffect(() => {
-      
-        const ven = async () => {
-            const res = await axios.get(`https://localhost:4000/vendors/${venid}`)
-            setVendor(res.data);
+  useEffect(() => {
+    const ven = async () => {
+      const res = await axios.get(`http://localhost:4000/vendors/${venid}`);
+      setVendor(res.data);
+    };
+    ven();
+  }, [venid]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/customers/${id}/notify/${venid}`)
+      .then((res) => {
+        if (res.data === "Done") {
+          console.log("Yes");
         }
-        ven();
-         
-        }, [venid])
+      });
+  }, [id, venid]);
 
-     
-
-     
-        useEffect(() => {
-  
-           
-          axios.get(`https://localhost:4000/customers/${id}/notify/${venid}`)
-          .then((res) => {
-              if(res.data === "Done")
-              {
-                console.log("Yes");
-             
-              }
-          })
-      
-        
-     
-        }, [id, venid])
-        
-
-
-     return (
+  return (
     <div>
-      <div className='containerss'>
-        <div className='info'>
-        <h1>Notification has been send to your costomer
-        :{customer.name}</h1>
-       
+      <div className="containerss">
+        <div className="info">
+          <h1>Notification has been send to your costomer :{customer.name}</h1>
         </div>
-        <div className='img' >
-          <img src={notifylogo} alt="notifylogo.jpg"/>
-          
-          
+        <div className="img">
+          <img src={notifylogo} alt="notifylogo.jpg" />
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Notify
+export default Notify;
